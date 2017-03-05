50e5f68ae809dbb8f266db6a1e8618fee73afaa4
Raymon de Looff <raydelooff@gmail.com>
2017-03-05 23:52:32 +0100

    Add support for lights

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const inherits = require('util').inherits;
 const HuePlatform = require('./lib/HuePlatform');
 
 let Accessory;
@@ -20,6 +21,20 @@ function Platform(log, config, api) {
   this.log = log;
   this.api = api;
   this.platform = new HuePlatform.HuePlatform(Accessory, Service, Characteristic, UUIDGen, log, config, api);
+
+  Characteristic.ColorTemperature = function () {
+    Characteristic.call(this, 'Color Temperature', 'E887EF67-509A-552D-A138-3DA215050F46');
+    this.setProps({
+      format: Characteristic.Formats.INT,
+      minValue: 153,
+      maxValue: 500,
+      minStep: 1,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  };
+
+  inherits(Characteristic.ColorTemperature, Characteristic);
 }
 
 Platform.prototype.configureAccessory = function (accessory) {
